07b530b05781986e7aaf5e6073f810f082495668
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-04-12 20:02:32 -0300

    Identações

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,11 @@
 function mudaFoto(foto) {
     document.getElementById("icone").src = foto;
 }
+
+function calc_total() {
+    let idQtd = document.getElementById("cQtd");
+    let qtd = parseInt(idQtd.value);
+    let valorGoogleGlass = 1500;
+    let qtdTotal = qtd * valorGoogleGlass;
+    document.getElementById("cTot").value = qtdTotal;
+}
